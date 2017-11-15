1dcbe6e62ba1bf1f9b3d47d870876bcb3a58c5b8
Scott Serage <xttocsurge51@gmail.com>
2017-11-15 15:17:54 -0700

    Enabled redux, created gIcon function

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,22 @@
 import axios from "axios";
 
 const initialState = {
-
+  gIcon: false
 }
+    , G_ICON_CHANGE = 'G_ICON_CHANGE';
+
+export function showGroup(bool){
+  return {
+    type: G_ICON_CHANGE,
+    payload: bool
+  };
+}
+
+export default function reducer(state = initialState, action){
+  switch (action.type){
+    case G_ICON_CHANGE:
+      return Object.assign({}, state, {gIcon: action.payload});
+    default:
+      return state;
+  }
+};
