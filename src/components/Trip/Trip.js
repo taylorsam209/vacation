e388cc4d9baddbc3eecff00079179e4a71b7ff6e
Scott Serage <xttocsurge51@gmail.com>
2017-11-15 22:35:36 -0700

    Restored the changes I lost due to carelessness (styling, datePicker...)

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -1,15 +1,89 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux'
+import './Trip.css';
+import {showGroup} from '../../ducks/reducer';
+/* Components*/
 import Menu from '../Menu/Menu.js';
+import RaisedButton from 'material-ui/RaisedButton';
+import DatePicker from 'material-ui/DatePicker';
+import Dialog from 'material-ui/Dialog';
 
 class Trip extends Component {
-    render() {
-        return (
-            <div>
-                <Menu />
-                <h1>Current Trip</h1>
-            </div>
-        )
+  constructor(props){
+    super(props);
+    this.state = {
+      open: false,
+      dayName: 'New Day'
     }
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.showGroup(true);
+  }
+
+  handleOpen(){
+    this.setState({open: true});
+  };
+
+  handleClose(){
+    this.setState({open: false});
+  };
+
+  updateDayName(value){
+    this.setState({dayName: value});
+  };
+
+  render() {
+    const {dayName} = this.state;
+    const actions =
+      <div className='new-day-actions'>
+        <RaisedButton
+          label="Ok"
+          primary={true}
+          onClick={this.handleClose}
+          className='new-day-ok'
+        />
+        <RaisedButton
+          label='Cancel'
+          secondary={true}
+          onClick={this.handleClose}
+          className='new-day-cancel'
+        />
+      </div>;
+
+    return (
+      <main>
+        <Menu/>
+        <section className='trip'>
+          <h1>Current Trip</h1>
+          <RaisedButton label="Add day" primary={true} onClick={this.handleOpen} />
+          <Dialog
+            title={dayName}
+            actions={actions}
+            modal={false}
+            open={this.state.open}
+            onRequestClose={this.handleClose}
+          >
+            <input
+              type='text'
+              placeholder={dayName}
+              onChange={(e)=>this.updateDayName(e.target.value)}
+            />
+            Select a date.
+            <DatePicker hintText="New adventure begins..." />
+          </Dialog>
+        </section>
+      </main>
+    )
+  }
 }
 
-export default Trip
+// function mapStateToProps(state){
+//   return {
+//
+//   }
+// };
+
+export default connect(null, {showGroup})(Trip);
