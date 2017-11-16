d37f56266bb3ce80f4e64084b8649923bfbdef27
Scott Serage <xttocsurge51@gmail.com>
2017-11-15 22:03:08 -0700

    Changed line spacing

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Menu from '../Menu/Menu.js';
 
 class Day extends Component {
-    render() {
-        return (
-            <div>
-                <Menu />
-                <h1>Day</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Menu/>
+        <h1>Day</h1>
+      </div>
+    );
+  }
 }
 
 export default Day;
