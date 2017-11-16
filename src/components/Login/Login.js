91d633f4cd1ea57ffd75693e1a6a3b76be47b66d
Scott Serage <xttocsurge51@gmail.com>
2017-11-16 09:49:17 -0700

    Added picture, started styling Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import About from '../About/About.js';
 import RaisedButton from 'material-ui/RaisedButton';
+import './Login.css';
 
 const style = {
   margin: 12
@@ -11,11 +12,11 @@ class Login extends Component {
     return (
       <main>
         <section className='login'>
-        <a href={process.env.REACT_APP_LOGIN}>
-          <RaisedButton label='Login / Sign-up' primary={true} style={style}/>
-        </a>
-        <h1>Login</h1>
-        <About/>
+          <a href={process.env.REACT_APP_LOGIN}>
+            <RaisedButton label='Login / Sign-up' primary={true} style={style}/>
+          </a>
+          <h1>Login</h1>
+          <About/>
         </section>
       </main>
     );
