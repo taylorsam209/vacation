3354f5cc08e953fd746779acbb88524eeec0fe00
Loganmcw <loganmcw90@gmail.com>
2017-11-16 16:30:02 -0700

    95% Component Setup; Needs Back-end Hook; Logan; 16Nov2017

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,13 +6,37 @@ import RaisedButton from 'material-ui/RaisedButton';
 import DatePicker from 'material-ui/DatePicker';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
 
 const richBlack = '#02111b';
 const dodgerBlue = '#1098f7';
 const green = '#00825D'
 
 class Dashboard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            tripName: '',
+            tripDetails: '',
+            tripCode: '',
+            tripDate: '',
+            tripLocation: ''
+        }
 
+        this.handleTrips = this.handleTrips.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleClosed = this.handleClose.bind(this);
+        this.validationName = this.validationName.bind(this);
+        this.validationCode = this.validationCode.bind(this);
+        this.handleTripName = this.handleTripName.bind(this);
+        this.handleTripDetails = this.handleTripDetails.bind(this);
+        this.handleTripCode = this.handleTripCode.bind(this);
+        this.handleTripDate = this.handleTripDate.bind(this);
+        this.handleTripLocation = this.handleTripLocation.bind(this);
+        this.tripDisableToggle = this.tripDisableToggle.bind(this);
+        this.newTrip = this.newTrip.bind(this);
+
+    }
 
     componentDidMount() {
         this.props.showGroup(false);
@@ -39,14 +63,112 @@ class Dashboard extends Component {
         this.props.newTripModal(false);
     };
 
+    handleTripName(e) {
+        this.setState({
+            tripName: e
+        })
+    }
+    handleTripDetails(e) {
+        this.setState({
+            tripDetails: e
+        })
+    }
+    handleTripCode(e) {
+        this.setState({
+            tripCode: e
+        })
+    }
+    handleTripDate(event, date) {
+        this.setState({
+            tripDate: date
+        })
+    }
+    handleTripLocation(e) {
+        this.setState({
+            tripLocation: e
+        })
+    }
+
+    validationName() {
+        if (this.state.tripName == '') {
+            return (
+                <div>
+                    <TextField required
+                        hintText="Trip Name"
+                        errorText="This field is required"
+                        onChange={e => { this.handleTripName(e.target.value) }}
+                    /> <br />
+                    <br />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <TextField required
+                        hintText="Trip Name"
+                        onChange={e => { this.handleTripName(e.target.value) }}
+                    /> <br />
+                    <br />
+                </div>
+            )
+        }
+    }
+    validationCode() {
+        if (this.state.tripCode.length !== 5) {
+            return (
+                <div>
+                    <TextField required
+                        hintText="Trip Code - Must Be Five Characters"
+                        errorText="This field is required and has to be five characters long"
+                        onChange={e => { this.handleTripCode(e.target.value) }}
+                        maxLength="5"
+                    /> <br />
+                    <br />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <TextField required
+                        hintText="Trip Code - Must Be Five Characters"
+                        onChange={e => { this.handleTripCode(e.target.value) }}
+                        maxLength="5"
+                    /> <br />
+                    <br />
+                </div>
+            )
+        }
+    }
+
+    tripDisableToggle() {
+        if (this.state.tripName !== '' && this.state.tripCode.length === 5) {
+            return false
+        } else {
+            return true
+        }
+    }
+
+    newTrip() {
+
+    }
+
     render() {
         const actions = [
-            <FlatButton
-                label="Ok"
-                primary={true}
-                keyboardFocused={true}
-                onClick={this.handleClose}
-            />,
+            <div className='new-day-actions'>
+                <RaisedButton
+                    label="Ok"
+                    primary={true}
+                    keyboardFocused={true}
+                    onClick={this.handleClose}
+                    disabled={this.tripDisableToggle()}
+                // onClick={() => { this.newTrip() }}
+                />
+                <RaisedButton
+                    label='Cancel'
+                    secondary={true}
+                    onClick={this.handleClose}
+                    className='new-day-cancel'
+                /></div>
         ];
         return (
             <div>
@@ -55,19 +177,30 @@ class Dashboard extends Component {
                 <RaisedButton
                     label='Plan A New Trip'
                     labelColor='white'
-                    backgroundColor={dodgerBlue}
+                    primary={true}
                     style={{ margin: '5px' }}
                     onClick={() => { this.handleOpen() }}
                 />
                 <Dialog
-                    title="Dialog With Date Picker"
+                    title="Tell us about your future trip!"
                     actions={actions}
                     modal={false}
                     open={this.props.newTripOpen}
                     onRequestClose={this.handleClose}
                 >
-                    Open a Date Picker dialog from within a dialog.
-                 <DatePicker hintText="Date Picker" />
+                    Pick a start date for your trip!
+                 <DatePicker hintText="Date Picker" onChange={(x, event) => { this.handleTripDate(x, event) }} />
+                    {this.validationName()}
+                    {this.validationCode()}
+                    <TextField
+                        hintText="Trip Location"
+                        onChange={e => { this.handleTripLocation(e.target.value) }}
+                    /> <br />
+                    <br /> <TextField
+                        hintText="Trip Details"
+                        onChange={e => { this.handleTripDetails(e.target.value) }}
+                    /> <br />
+                    <br />
                 </Dialog>
                 <div>{this.handleTrips()}</div>
             </div>
