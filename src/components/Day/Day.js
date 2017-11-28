c9ee3014b73951037062d1e8b9d83c802e837014
Loganmcw <loganmcw90@gmail.com>
2017-11-28 15:56:54 -0700

    Temp Add

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -14,7 +14,8 @@ import ActionCancel from 'material-ui/svg-icons/navigation/cancel';
 import { addEvent, getAllEvents, deleteEvent } from '../../ducks/frontEndABs.js';
 import { Link } from "react-router-dom";
 import { searchRestaurants } from "../../ducks/restaurant.js"
-
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Edit from 'material-ui/svg-icons/image/edit'
 
 class Day extends Component {
   constructor(props) {
@@ -25,7 +26,6 @@ class Day extends Component {
       value: 1,
       inputOne: '',
       inputTwo: '',
-
     }
     this.handleOpen = this.handleOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
@@ -35,7 +35,6 @@ class Day extends Component {
     this.handleAddEvent = this.handleAddEvent.bind(this);
     this.handleGetAllEvents = this.handleGetAllEvents.bind(this);
     this.handleEventDelete = this.handleEventDelete.bind(this);
-    // this.handleRestarauntSearch = this.handleRestarauntSearch.bind(this);
     this.handleRestaurants = this.handleRestaurants.bind(this);
   }
 
@@ -45,21 +44,38 @@ class Day extends Component {
   }
 
   handleAddEvent() {
-    const eventArr = [this.state.eventName, this.state.inputOne, this.state.inputTwo, this.state.value]
+    const eventArr = [this.state.eventName, this.state.inputOne, this.state.inputTwo]
     this.props.addEvent(eventArr)
+  }
+
+  handleAddRestaurant() {
+    const restArr = [this.state.eventName, this.props.currentRestaurant.name]
+    this.props.addRestaurant(restArr)
   }
 
   handleGetAllEvents() {
     // return this.props.eventsArr.map((e, i, arr) => {
-    //       return (
-    //           <div key={i}>
-    //               {e}
-    //               <IconButton tooltip="top-center" touch={true} tooltipPosition="top-center" onClick={() => { this.handleEventDelete() }}>
-    //                   <ActionCancel />
-    //               </IconButton>
-    //           </div>
-    //       )
-    //   })
+    //   return (
+    //     <div key={i}>
+    // <Card className='' style={{ margin: '10px', padding: '10px' }}>
+    //       {e.confirmation || null}
+    //       {e.airline_name || null}
+    //       {e.lodging_name || null}
+    //       {e.lodging_details || null}
+    //       {e.activity_name || null}
+    //       {e.activity_details || null}
+    //       {e.rental_company || null}
+    //       {e.rental_details || null}
+    //       <IconButton tooltip="Cancel Event" touch={true} tooltipPosition="top-center" onClick={() => { this.handleEventDelete(e) }}>
+    //         <ActionCancel />
+    //       </IconButton>
+    //       <IconButton tooltip="Edit Event" touch={true} tooltipPosition="top-center" onClick={() => { this.handleEventEdit(e) }}>
+    //         <Edit />
+    //       </IconButton>
+    // </Card>
+    //     </div>
+    //   )
+    // })
   }
 
   handleRestaurants() {
@@ -67,10 +83,16 @@ class Day extends Component {
     //   return resp.map((e, i, arr) => {
     //     return (
     //       <div key={i}>
-    //         {e}
-    //         <IconButton tooltip="top-center" touch={true} tooltipPosition="top-center" onClick={() => { this.handleEventDelete() }}>
+    //        <Card className='' style={{ margin: '10px', padding: '10px' }}>
+    //         <Link to=`/restaraunt/${e.yelpId}`>{e.title}</Link>
+    //           <img src={e.image_url || 'https://pixy.org/images/placeholder.png'} alt="" />
+    //         <IconButton tooltip="top-center" touch={true} tooltipPosition="top-center" onClick={() => { this.handleEventDelete(e) }}>
     //           <ActionCancel />
     //         </IconButton>
+    //       <IconButton tooltip="Edit Event" touch={true} tooltipPosition="top-center" onClick={() => { this.handleEventEdit(e) }}>
+    //         <Edit />
+    //       </IconButton>
+    //        </Card>
     //       </div>
     //     )
     //   })
@@ -81,6 +103,28 @@ class Day extends Component {
     this.props.deleteEvent(e)
   }
 
+  handleEventEdit(e) {
+    if (e.confirmation) {
+      this.handleOpen();
+      this.updateInputOne(e.confirmation);
+      this.updateInputTwo(e.airline_name);
+    } else if (e.lodging_name) {
+      this.handleOpen();
+      this.updateInputOne(e.lodging_name);
+      this.updateInputTwo(e.airline_details);
+    } else if (e.activity_name) {
+      this.handleOpen();
+      this.updateInputOne(e.activity_name);
+      this.updateInputTwo(e.activity_details);
+    } else if (e.rental_company) {
+      this.handleOpen();
+      this.updateInputOne(e.rental_company);
+      this.updateInputTwo(e.rental_details);
+    } else if (e.yelpId) {
+      this.handleOpen();
+      this.updateInputOne(e.location);
+    }
+  }
 
   handleChange = (event, index, value) => this.setState({ value });
 
@@ -99,10 +143,6 @@ class Day extends Component {
       inputTwo: ''
     })
   }
-
-  // handleRestarauntSearch() {
-
-  // }
 
   updateEventName(value) {
     this.setState({ eventName: value });
@@ -173,7 +213,7 @@ class Day extends Component {
       return (
         <div>
           <TextField
-            hintText="Restaraunt Name"
+            hintText="Enter City"
             id="text-field-default-event"
             defaultValue={this.state.inputOne}
             onChange={(e) => this.updateInputOne(e.target.value)}
@@ -222,6 +262,22 @@ class Day extends Component {
         />
       </div>
     );
+    const restaurantActions = (
+      <div className='new-event-actions'>
+        <RaisedButton
+          label="Ok"
+          primary={true}
+          onClick={() => { this.handleAddRestaurant(), this.props.closeRestaurantModal() }}
+          className='new-event-ok'
+        />
+        <RaisedButton
+          label='Cancel'
+          secondary={true}
+          onClick={this.handleClose}
+          className='new-event-cancel'
+        />
+      </div>
+    );
     return (
       <main>
         <Menu />
@@ -251,7 +307,26 @@ class Day extends Component {
               onChange={(e) => this.updateEventName(e.target.value)}
             />
             {this.handleEventType()}
-          </Dialog>
+          </Dialog >
+          {/* <Dialog
+            title={eventName}
+            actions={restaurantActions}
+            modal={false}
+            open={this.props.restaurantModalToggle}
+            onRequestClose={this.props.closeRestaurantModal()}>
+            <div>
+              <TextField
+                id="text-field-default-event"
+                defaultValue={eventName}
+                onChange={(e) => this.updateEventName(e.target.value)}
+              />
+              <TextField
+                hintText={this.props.currentRestaurant.name}
+                id="text-field-default-event"
+                defaultValue={this.props.currentRestaurant.name}
+              />
+            </div>
+          </Dialog> */}
         </section>
       </main>
     )
@@ -259,7 +334,9 @@ class Day extends Component {
 }
 function mapStateToProps(state) {
   return {
-    gIcon: state.gIcon
+    gIcon: state.gIcon,
+    restaurantModalToggle: state.frontEnd.restaurantModalToggle,
+    currentRestaurant: state.restaurant.currentRestaurant
   }
 }
 
