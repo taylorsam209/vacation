6b6ad4f9569dd8d8bbc6ea1acf81e0079feef7cf
Scott Serage <xttocsurge51@gmail.com>
2017-11-28 22:27:39 -0700

    Styled about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import './About.css';
 
 export default function About(props) {
-    return (
-        <h1>About</h1>
-    )
+  return (
+    <section className='about'>
+      <div className='about-content'>
+        <h1 className='about-header'>What's Trippin'?</h1>
+        <p>
+          Lorem ipsum dolor sit amet, est no aliquid eruditi, ea adhuc aperiri accusamus vix, primis docendi signiferumque eam at. Mea lobortis adipiscing concludaturque ut. Habeo eripuit id pri, mei cu bonorum nonumes interesset, vim aliquid consetetur consequuntur ne. Est ad impedit consulatu expetendis, cibo dolores adversarium an mei. An vix delenit epicuri dolores, in dicta philosophia vix, ea nec rebum virtute labores. Eam ei quidam vivendum. Ex eam dolorem persequeris, mei et antiopam platonem.
+        </p>
+
+      </div>
+    </section>
+  )
 }
+
+// <p>
+//   Detracto consequuntur no pri. Sea ea prima conceptam. Ei insolens explicari voluptaria duo. Impedit forensibus ei nam, mel odio quot aliquip eu. Aperiam complectitur et sed, alia aperiam eum ad. Quod illud mea ut, unum patrioque hendrerit et pri, te equidem dolorum qualisque ius.
+// </p>
+// <p>
+//   Sanctus posidonium persequeris ad has, solet splendide referrentur ei cum. Cum verear iudicabit repudiandae et, qui ei mutat tractatos. Nam et omittam salutatus, sea et affert consulatu forensibus. In ferri maiorum verterem sed, per tincidunt sadipscing te. Mea fugit summo eu, elitr efficiendi ne sit. Persius explicari an ius, nulla erant usu cu.
+// </p>
+// <p>
+//   Cum et fabulas pertinax. Nullam abhorreant eum id, delicata partiendo an vim. At dico praesent assentior est, per no wisi malorum. Vim eu commodo vocibus. Error legere audiam ut vix, eum te tollit verear intellegat, adipiscing quaerendum an sed. His lorem quaestio ex. Eu vero minim libris quo.
+// </p>
